b3ee04fe9146697b1e927ea1224869ccac394e2d
Luke Porupski <l.porupski@outlook.com>
2021-07-09 15:02:05 -0700

    cleanup

--- koi/models.py
+++ koi/models.py
@@ -244,17 +244,6 @@ class CB_Account(NamedTuple):
     profile_id: str
     trading_enabled: bool
 
-    # Example
-    # {
-    #     "id": "e316cb9a-0808-4fd7-8914-97829c1925de",
-    #     "currency": "USD",
-    #     "balance": "80.2301373066930000",
-    #     "available": "79.2266348066930000",
-    #     "hold": "1.0035025000000000",
-    #     "profile_id": "75da88c5-05bf-4f54-bc85-5c775bd68254",
-    #     "trading_enabled": true
-    # }
-
     @classmethod
     def from_json(cls, data: dict):
         return cls(**data)
